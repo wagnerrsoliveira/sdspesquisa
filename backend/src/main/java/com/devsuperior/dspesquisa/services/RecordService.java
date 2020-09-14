e7175e8326e6779aa4be94b1ec7e8bc6052cfc99
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-09-14 15:55:57 -0300

    GET records

--- backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
@@ -3,6 +3,8 @@ package com.devsuperior.dspesquisa.services;
 import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,6 +36,12 @@ public class RecordService {
 		
 		record = recordRepository.save(record);
 		return new RecordDTO(record);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<RecordDTO> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+		
+		return recordRepository.findByMoments(minDate,maxDate,pageRequest).map(record-> new RecordDTO(record));
 	}
 
 }
